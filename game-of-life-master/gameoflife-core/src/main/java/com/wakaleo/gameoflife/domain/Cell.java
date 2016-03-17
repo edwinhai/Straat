b394ed7723ef7c1396c2125f4d47bc7e07bdaea4
edwinhai <edwinhai@gmail.com>
2016-03-17 11:20:36 +0100

    test field changed in cell

--- game-of-life-master/gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ game-of-life-master/gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -4,7 +4,7 @@ public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
 
     private String symbol;
-    private String test2;
+    private String test23;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
